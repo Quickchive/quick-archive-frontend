b1ba1a2d76bdf07d1d8cc92e5b43a95bceadc86e
oseunghyun <ohsh1477@gmail.com>
2023-07-15 00:14:18 +0900

    style: 홈-leftNav 컴포넌트 ui 구현 #4

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
     {
       path: '/about',
       name: 'about',
@@ -24,6 +18,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../components/login/LoginComponent.vue')
+    },
+    {
+      path: '/home',
+      name: 'home',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../Views/HomeView.vue')
     }
   ]
 })
