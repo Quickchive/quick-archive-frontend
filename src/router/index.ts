ad38cb27f8976b71b8972fc3ac81bf105b3ea92a
oseunghyun <ohsh1477@gmail.com>
2023-07-18 08:18:47 +0900

    style: 콘텐츠 컴포넌트 ui 구현 #4

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,20 +4,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
       path: '/login',
       name: 'login',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../components/login/LoginComponent.vue')
+      component: () => import('@/components/login/LoginComponent.vue')
     },
     {
       path: '/home',
@@ -25,7 +17,12 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../Views/HomeView.vue')
+      component: () => import('@/Views/HomeView.vue')
+    },
+    {
+      path: '/testpage',
+      name: 'test',
+      component: () => import('@/components/home/ContentsItem.vue')
     }
   ]
 })
