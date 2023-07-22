f09cf673448dc0393c2a27c44e5083c5c5200e41
oseunghyun <ohsh1477@gmail.com>
2023-07-22 10:46:35 +0900

    style: 카테고리/콘텐츠 추가 모달 ui 구현 #6

---
+++ src/components/category/CategoryModalAdd.vue
@@ -0,0 +1,25 @@
+<template>
+  <dialog class="flex-container__col category-add__modal">
+    <header class="flex-container__row">
+      <h1>추가</h1>
+      <button class="button--transparent close-button--top"><img :src="topCloseIcon" /></button>
+    </header>
+    <div class="flex-container__row add-category__button__wrapper">
+      <button class="flex-container__col add-category__button">콘텐츠</button>
+      <button class="flex-container__col add-category__button">카테고리</button>
+    </div>
+  </dialog>
+</template>
+
+<script>
+import topCloseIcon from '@/assets/ic/ic-top-close.svg'
+export default {
+  data() {
+    return {
+      topCloseIcon
+    }
+  }
+}
+</script>
+
+<style></style>
