74b3bbc5671324e2a4ff0567bf3d917ae3e005f4
oseunghyun <ohsh1477@gmail.com>
2023-07-29 17:11:15 +0900

    style: 카테고리 추가-완료 토스트 ui 구현 #6

---
+++ src/components/toast/ToastDefault.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex-container__row toast">
+    <img :src="doneIcon" />
+    <span class="toast-content">{{ toastText }}</span>
+    <button class="toast-button">보러가기</button>
+  </div>
+</template>
+
+<script>
+import doneIcon from '@/assets/ic/ic-done.svg'
+export default {
+  data() {
+    return {
+      doneIcon,
+      toastText: '카테고리가 추가되었습니다.'
+    }
+  }
+}
+</script>
+
+<style></style>
