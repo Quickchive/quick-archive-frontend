302edfdda4aa2d2bba043a7f46827903184b8260
oseunghyun <ohsh1477@gmail.com>
2023-09-05 22:30:47 +0900

    design: 로그인 페이지 딤드 처리 및 위치 조정 #26

--- src/components/login/LoginComponent.vue
+++ src/components/login/LoginComponent.vue
@@ -1,22 +1,39 @@
 <template>
-  <div class="flex-container__col login-container">
-    <div class="flex-container__row close-btn__wrapper">
-      <button class="button--transparent"><img :src="icClose" /></button>
+  <div class="overlay"></div>
+  <div class="login-container">
+    <div class="flex-container__col login-form">
+      <div class="flex-container__row close-btn__wrapper">
+        <button class="button--transparent"><img :src="icClose" /></button>
+      </div>
+      <header class="header__login">
+        <h1>Login</h1>
+        <p>간단하게 로그인하고 퀵카이브를 이용해보세요</p>
+      </header>
+      <div class="banner__wrapper">
+        <img :src="union" />
+        <span><strong>3초만에</strong> 빠른 회원가입!</span>
+      </div>
+      <div class="flex-container__col login-btn__wrapper">
+        <button class="button button--kakao">
+          <img :src="logoKakao" />
+          <a href="https://test.quickarchive.co.kr/api/oauth/kakao-auth" target="_blank"
+            >카카오톡으로 시작하기</a
+          >
+        </button>
+        <button class="button button--google">
+          <img :src="logoGoogle" />
+          <a href="https://test.quickarchive.co.kr/api/oauth/google-auth" target="_blank"
+            >Google로 시작하기</a
+          >
+        </button>
+        <button class="button button--apple"><img :src="logoApple" />Apple로 시작하기</button>
+      </div>
+      <div class="desc__wrapper">
+        <a href="https://www.notion.so/fabc4b39c85b446bab8f2bb8549926b7"
+          >서비스 이용약관 및 개인정보처리방침</a
+        ><span> 안내</span>
+      </div>
     </div>
-    <header class="header__login">
-      <h1>Login</h1>
-      <p>간단하게 로그인하고 퀵카이브를 이용해보세요</p>
-    </header>
-    <div class="banner__wrapper">
-      <img :src="union" />
-      <span><strong>3초만에</strong> 빠른 회원가입!</span>
-    </div>
-    <div class="flex-container__col login-btn__wrapper">
-      <button class="button button--kakao"><img :src="logoKakao" />카카오톡으로 시작하기</button>
-      <button class="button button--google"><img :src="logoGoogle" />Google로 시작하기</button>
-      <button class="button button--apple"><img :src="logoApple" />Apple로 시작하기</button>
-    </div>
-    <div class="desc__wrapper"><a>서비스 이용약관 및 개인정보처리방침</a><span> 안내</span></div>
   </div>
 </template>
 
@@ -35,6 +52,25 @@ export default {
       logoGoogle,
       logoApple,
       union
+    }
+  },
+  methods: {
+    // 로그인 요청
+    async submitForm() {
+      try {
+        const userData = {
+          email: this.email,
+          password: this.pw,
+          auto_login: this.stayLogin
+        }
+        await this.$store.dispatch('LOGIN', userData)
+        // 로그인 유지 여부 저장
+        this.$store.dispatch('STAY_LOGIN', this.stayLogin)
+        await this.$router.push('/main').catch(() => {})
+      } catch (error) {
+        this.alertModalContent = error.response.data.message
+        this.isAlertModalActive = true
+      }
     }
   }
 }
