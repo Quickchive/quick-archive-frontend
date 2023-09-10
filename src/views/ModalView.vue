a370fdc5cd40c15bd8a6e18da528ea9f239ed93c
oseunghyun <ohsh1477@gmail.com>
2023-09-10 23:25:05 +0900

    design: 로그인 모달 위치 조정 #26

---
+++ src/views/ModalView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="overlay"></div>
+  <div class="modal-view">
+    <modal-select-category-or-content></modal-select-category-or-content>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    ModalSelectCategoryOrContent
+  }
+}
+
+import ModalSelectCategoryOrContent from '@/components/modal/ModalSelectCategoryOrContent.vue'
+</script>
+
+<style></style>
