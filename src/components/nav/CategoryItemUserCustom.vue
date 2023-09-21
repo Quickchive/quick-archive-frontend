3821a7d894f1fc3463c4ef3ef2d9dc1df3906cf6
oseunghyun <ohsh1477@gmail.com>
2023-09-21 19:40:39 +0900

    fix: 사용자 카테고리 매핑 작업 #27

--- src/components/nav/CategoryItemUserCustom.vue
+++ src/components/nav/CategoryItemUserCustom.vue
@@ -1,38 +1,38 @@
 <template>
   <div class="category-item">
     <!-- 1차 카테고리 -->
-    <ul v-if="categoryData.firstCategoryName" class="category-list__first-ul">
+    <ul v-if="categoryData.name" class="category-list__first-ul">
       <li>
         <div class="flex-container__row--align-center">
           <button class="button--transparent expand-button">
-            <img v-if="categoryData.secondCategoryName" :src="expandLessIcon" />
-            <img v-if="!categoryData.secondCategoryName" :src="expandMoreIcon" />
+            <img v-if="categoryData.children" :src="expandLessIcon" />
+            <img v-if="!categoryData.children" :src="expandMoreIcon" />
           </button>
           <button class="button--transparent category-list__button">
             <img :src="categoryIcon" class="category-icon img-category-icon" />
-            {{ categoryData.firstCategoryName }}
+            {{ categoryData.name }}
           </button>
         </div>
         <button class="button--transparent"><img :src="moreIcon" /></button>
       </li>
       <!-- 2차 카테고리 -->
-      <ul v-if="categoryData.secondCategoryName">
+      <ul v-if="categoryData.children">
         <li>
           <div class="flex-container__row--align-center">
             <button class="button--transparent expand-button">
-              <img v-if="categoryData.thirdCategoryName" :src="expandLessIcon" />
-              <img v-if="!categoryData.thirdCategoryName" :src="expandMoreIcon" />
+              <img v-if="categoryData.children.children" :src="expandLessIcon" />
+              <img v-if="!categoryData.children.children" :src="expandMoreIcon" />
             </button>
             <button class="button--transparent category-list__button">
               <img :src="categoryIcon" class="category-icon img-category-icon" />{{
-                categoryData.secondCategoryName
+                categoryData.children.name
               }}
             </button>
           </div>
           <button class="button--transparent"><img :src="moreIcon" /></button>
         </li>
         <!-- 3차 카테고리 -->
-        <ul v-if="categoryData.thirdCategoryName">
+        <ul v-if="categoryData.children.children">
           <li>
             <div class="flex-container__row--align-center">
               <button class="button--transparent expand-button">
@@ -40,7 +40,7 @@
               </button>
               <button class="button--transparent category-list__button">
                 <img :src="categoryIcon" class="category-icon img-category-icon" />{{
-                  categoryData.thirdCategoryName
+                  categoryData.children.children.name
                 }}
               </button>
             </div>
