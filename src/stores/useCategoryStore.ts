826e910e4267cccb74efd6fbee38b377b201e9c3
oseunghyun <ohsh1477@gmail.com>
2023-11-05 20:16:23 +0900

    refactor: categoryStore의 문법을 옵션 스토어에서 셋업 스토어로 변경

--- src/stores/useCategoryStore.ts
+++ src/stores/useCategoryStore.ts
@@ -2,81 +2,88 @@ import { defineStore } from 'pinia'
 import { getCategories } from '@/api/category'
 import { deleteCategories } from '@/api/category.js'
 import { useModalStore } from '@/stores/useModalStore.ts'
+import { ref } from 'vue'
 
-const modalStore = useModalStore()
+export const useCategoryStore = defineStore('category', () => {
+  const modalStore = useModalStore()
 
-export const useCategoryStore = defineStore('category', {
-  // 화살표 함수는 전체 유형 유추을 위해 권장됨.
-  state: () => {
-    return {
-      userCategoryList: [
-        {
-          id: 0,
-          createdAt: '2023-09-20T14:43:26.954Z',
-          updatedAt: '2023-09-20T14:43:26.954Z',
-          name: '쇼핑',
-          slug: '쇼핑',
-          parentId: null,
-          userId: 7,
-          iconName: 'Cake'
-        },
-        {
-          id: 1,
-          createdAt: '2023-09-20T23:36:28.027Z',
-          updatedAt: '2023-09-20T23:36:28.027Z',
-          name: '꿀팁',
-          slug: '꿀팁',
-          parentId: null,
-          userId: 7,
-          iconName: 'Watch'
-        }
-      ],
-      curCategoryName: '전체 콘텐츠',
-      focusedCategoryId: -1,
-      focusedCategoryData: {},
-      deleteContentsChecked: false
+  const userCategoryList = ref([
+    {
+      id: 0,
+      createdAt: '2023-09-20T14:43:26.954Z',
+      updatedAt: '2023-09-20T14:43:26.954Z',
+      name: '쇼핑',
+      slug: '쇼핑',
+      parentId: null,
+      userId: 7,
+      iconName: 'Cake'
+    },
+    {
+      id: 1,
+      createdAt: '2023-09-20T23:36:28.027Z',
+      updatedAt: '2023-09-20T23:36:28.027Z',
+      name: '꿀팁',
+      slug: '꿀팁',
+      parentId: null,
+      userId: 7,
+      iconName: 'Watch'
     }
-  },
-  getters: {
-    getFocusedCategoryId: (state) => state.focusedCategoryId
-  },
-  actions: {
-    async getUserCategoryList() {
-      try {
-        const response = await getCategories()
-        console.log('store', response.data.categoriesTree)
-        if (response.data.statusCode == 200) {
-          this.userCategoryList = response.data.categoriesTree
-        }
-      } catch (error) {
-        console.log(error)
-      }
-    },
-    setCategoryName(categoryName: string) {
-      this.curCategoryName = categoryName
-    },
-    setFocusedCategory(categoryId: number) {
-      this.focusedCategoryId = categoryId
-    },
-    setFocusedCategoryData(categoryData: object) {
-      this.focusedCategoryData = categoryData
-    },
+  ])
+  const curCategoryName = ref('전체 콘텐츠')
+  const focusedCategoryId = ref(-1)
+  const focusedCategoryData = ref({})
+  const deleteContentsChecked = ref(false)
 
-    async deleteCategory() {
-      try {
-        const focusedCategoryId = this.getFocusedCategoryId
-        const response = await deleteCategories(focusedCategoryId, this.deleteContentsChecked)
-        console.log(response)
-        if (response.data.statusCode === (200 || 201)) {
-          this.getUserCategoryList()
-          modalStore.closeDeleteCategoryModal()
-        }
-      } catch (error) {
-        console.log(error)
+  async function getUserCategoryList() {
+    try {
+      const response = await getCategories()
+      console.log('store', response.data.categoriesTree)
+      if (response.data.statusCode == 200) {
+        userCategoryList.value = response.data.categoriesTree
       }
-    },
-    deleteContentsInCategory() {
-      this.deleteContentsChecked = !this.deleteContentsChecked
+    } catch (error) {
+      console.log(error)
     }
+  }
+  function setCategoryName(categoryName: string) {
+    curCategoryName.value = categoryName
+  }
+
+  function setFocusedCategory(categoryId: number) {
+    focusedCategoryId.value = categoryId
+  }
+
+  function setFocusedCategoryData(categoryData: object) {
+    focusedCategoryData.value = categoryData
+  }
+
+  async function deleteCategory() {
+    try {
+      const focusedCategory_id = focusedCategoryId.value
+      const response = await deleteCategories(focusedCategory_id, deleteContentsChecked)
+      console.log(response)
+      if (response.data.statusCode === (200 || 201)) {
+        getUserCategoryList()
+        modalStore.closeDeleteCategoryModal()
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  function deleteContentsInCategory() {
+    deleteContentsChecked.value = !deleteContentsChecked.value
+  }
+  return {
+    userCategoryList,
+    curCategoryName,
+    focusedCategoryId,
+    focusedCategoryData,
+    deleteContentsChecked,
+    getUserCategoryList,
+    deleteContentsInCategory,
+    deleteCategory,
+    setCategoryName,
+    setFocusedCategory,
+    setFocusedCategoryData
   }
 })
