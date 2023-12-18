f69c39504d476ec7082bedb247cadfb261653336
oseunghyun <ohsh1477@gmail.com>
2023-12-18 22:28:42 +0900

    feat: 콘텐츠 메모 더보기 기능/접기 구현 #48

--- src/components/home/ContentsItem.vue
+++ src/components/home/ContentsItem.vue
@@ -7,18 +7,43 @@
         <img :src="favoriteUnselectedIcon" v-if="!item.favorite" />
       </button>
     </div>
-    <article class="item__wrapper">
+    <footer class="item__wrapper">
       <h1>{{ item.title }}</h1>
       <p>{{ item.description }}</p>
       <footer class="flex-container__row">
-        <img class="ic-category" :src="categoryIcon" />
-        {{ item.category || '미지정' }}
+        <div id="thumbnail-category" @click="toCategoryPage(item.category.id)">
+          <img class="ic-category" :src="categoryIcon" />
+          {{ item.category ? item.category.name : '미지정' }}
+        </div>
+
         <img :src="divider" />
         {{ item.siteName }}
       </footer>
-    </article>
+    </footer>
     <button class="button--transparent btn--more"><img :src="moreIcon" /></button>
   </article>
+  <!-- <div class="flex-container__row item-container"> -->
+  <article v-if="item.comment" class="searhResult-content-memo">
+    <h1 class="searhResult-content-memo__title">메모</h1>
+    <p
+      class="searhResult-content-memo__content"
+      ref="paragraphElement"
+      :class="{ hideMemo: !isMemoShowAll }"
+    >
+      {{ item.comment }}
+    </p>
+    <button class="searchResult-content-memo__button" v-if="isMemoShowAll" @click="toggleMemo()">
+      접기<img :src="viewFoldIcon" />
+    </button>
+    <button
+      class="searchResult-content-memo__button"
+      v-if="paragraphHeight >= 44 && !isMemoShowAll"
+      @click="toggleMemo()"
+    >
+      더보기<img :src="viewMoreIcon" />
+    </button>
+  </article>
+  <!-- </div> -->
 </template>
 
 <script setup>
@@ -29,8 +54,13 @@ import favoriteSelectedIcon from '@/assets/ic/ic-favorite-seleted_32px.svg'
 import favoriteUnselectedIcon from '@/assets/ic/ic-favorite-unseleted_32px.svg'
 import { addFavorite } from '@/api/contents.js'
 import { useContentStore } from '@/stores/useContentStore.ts'
+import { useRouter } from 'vue-router'
+import viewMoreIcon from '@/assets/ic/ic_view-more.svg'
+import viewFoldIcon from '@/assets/ic/ic_view-fold.svg'
+import { onMounted, ref } from 'vue'
 
 const contentStore = useContentStore()
+const router = useRouter()
 
 const props = defineProps({
   item: Object
@@ -47,6 +77,24 @@ const favoriteEvent = async () => {
     console.log(error)
   }
 }
-</script>
 
+const toCategoryPage = (categoryId) => {
+  router.push(`/home/${categoryId}`)
+}
+
+const isMemoShowAll = ref(false)
+const paragraphElement = ref(null)
+const paragraphHeight = ref(22)
+
+onMounted(() => {
+  if (paragraphElement.value) {
+    const computedStyle = window.getComputedStyle(paragraphElement.value)
+    paragraphHeight.value = parseInt(computedStyle.height, 10)
+  }
+})
+
+const toggleMemo = () => {
+  isMemoShowAll.value = !isMemoShowAll.value
+}
+</script>
 <style></style>
