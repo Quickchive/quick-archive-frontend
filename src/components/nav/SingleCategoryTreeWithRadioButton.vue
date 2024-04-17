b793bae8f76915510ec46e77438e36f7d910954d
oseunghyun <ohsh1477@gmail.com>
2024-04-17 21:42:16 +0900

    style: 카테고리를 선택하는 모달에서 클릭 영역 수정
    [내용]
    - 카테고리 아이콘부터 라디오 버튼 사이 어느 곳을 클릭해도 라디오 버튼이 활성화 되도록 범위 수정

--- src/components/nav/SingleCategoryTreeWithRadioButton.vue
+++ src/components/nav/SingleCategoryTreeWithRadioButton.vue
@@ -1,34 +1,36 @@
 <template>
   <li :class="props.treeWidth">
-    <div class="flex-container__row--align-center">
+    <div class="singleCategoryTreeWithRadio">
       <button class="btn--transparent expand-button" @click="clickArrowEvent()">
         <img v-if="isChildrenCategoryShow" :src="expandLessIcon" />
         <img v-if="!isChildrenCategoryShow" :src="expandMoreIcon" />
       </button>
-      <button class="btn--transparent category-list__btn--radio">
-        <img
-          :src="
-            props.category.iconName == undefined
-              ? modalDataStore.getCategoryImgByIconName('Book')
-              : modalDataStore.getCategoryImgByIconName(props.category.iconName)
-          "
-          class="category-icon img-category-icon--lg"
-        />
-        {{ props.category.name }}
-      </button>
-    </div>
-    <!-- 라디오 버튼 -->
-    <div class="radio-buttons">
-      <label :for="props.category.id" class="radio-button">
-        <input
-          type="radio"
-          :id="props.category.id"
-          name="grades"
-          :checked="props.category.name === modalDataStore.selectedLocation.name"
-          @click="selectRadioButton(props.category)"
-        />
-        <span class="custom-radio"></span>
-      </label>
+
+      <div class="category-list__btn-wrapper" @click="selectRadioButton(props.category)">
+        <button class="btn--transparent category-list__btn--radio">
+          <img
+            :src="
+              props.category.iconName == undefined
+                ? modalDataStore.getCategoryImgByIconName('Book')
+                : modalDataStore.getCategoryImgByIconName(props.category.iconName)
+            "
+            class="category-icon img-category-icon--lg"
+          />
+          {{ props.category.name }}
+        </button>
+        <!-- 라디오 버튼 -->
+        <div class="radio-buttons">
+          <label :for="props.category.id" class="radio-button">
+            <input
+              type="radio"
+              :id="props.category.id"
+              name="grades"
+              :checked="props.category.name === modalDataStore.selectedLocation.name"
+            />
+            <span class="custom-radio"></span>
+          </label>
+        </div>
+      </div>
     </div>
   </li>
 </template>
