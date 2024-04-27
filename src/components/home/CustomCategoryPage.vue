336361357eae18596a3db959a5676263eb607245
oseunghyun <ohsh1477@gmail.com>
2024-04-27 15:46:32 +0900

    style: CustomCategoryPage의 불필요한 div 때문에 발생하는 여백 이슈 수정

--- src/components/home/CustomCategoryPage.vue
+++ src/components/home/CustomCategoryPage.vue
@@ -1,28 +1,26 @@
 <template>
-  <div>
-    <title-bar></title-bar>
-    <div class="divider"></div>
-    <article class="flex-container__col contents-container">
-      <div class="contents-num__wrapper">{{ contentStore.userCustomContentList.length }}개</div>
-      <!-- 콘텐츠 있을 때 -->
-      <template v-if="contentStore.userCustomContentList.length > 0">
-        <div class="flex-container__col contents-list__wrapper">
-          <contents-item
-            v-for="item in contentStore.userCustomContentList"
-            :item="item"
-            :key="item"
-          ></contents-item>
-        </div>
-      </template>
-      <!-- 콘텐츠 없을 떄 -->
-      <template v-else>
-        <div class="flex-container__col contents__empty">
-          <img :src="emptyImg" class="img-empty" />
-          <span>저장된 콘텐츠가 없습니다.</span>
-        </div>
-      </template>
-    </article>
-  </div>
+  <title-bar></title-bar>
+  <div class="divider"></div>
+  <article class="contents-container">
+    <div class="contents-num__wrapper">{{ contentStore.userCustomContentList.length }}개</div>
+    <!-- 콘텐츠 있을 때 -->
+    <template v-if="contentStore.userCustomContentList.length > 0">
+      <div class="contents-list__wrapper">
+        <contents-item
+          v-for="item in contentStore.userCustomContentList"
+          :item="item"
+          :key="item"
+        ></contents-item>
+      </div>
+    </template>
+    <!-- 콘텐츠 없을 떄 -->
+    <template v-else>
+      <div class="flex-container__col contents__empty">
+        <img :src="emptyImg" class="img-empty" />
+        <span>저장된 콘텐츠가 없습니다.</span>
+      </div>
+    </template>
+  </article>
 </template>
 
 <script setup>
