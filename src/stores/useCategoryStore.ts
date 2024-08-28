f2de18afd924a1aa6f5fa6b2fb16c600c2906ac3
oseunghyun <ohsh1477@gmail.com>
2024-08-28 21:28:24 +0900

    fix: “카테고리가 삭제되었습니다” 토스트 내 “보러가기” 버튼 삭제

--- src/stores/useCategoryStore.ts
+++ src/stores/useCategoryStore.ts
@@ -318,10 +318,10 @@ export const useCategoryStore = defineStore('category', () => {
       await updateUserCategoryList()
       modalViewStore.hideModalWithOverlay('deleteCategory', 'default')
       const toastData = {
-        message: '카테고리가 삭제되었습니다.'
-        // func: {
-        //   message: '취소하기'
-        // }
+        message: '카테고리가 삭제되었습니다.',
+        func: {
+          message: ''
+        }
       }
       toastStore.executeDefaultToast(toastData)
     } catch (error: any) {
