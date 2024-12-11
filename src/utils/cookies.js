b4c47990d2775ad79c66c8ec3b3488ddc2d130e4
oseunghyun <ohsh1477@gmail.com>
2024-12-11 19:53:18 +0900

    fix: 카테고리 자동 추천 설정 쿠키가 로그아웃 시에도 만료되지 않도록 수정

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -14,7 +14,7 @@ function saveHideAlertToCookie(value) {
 }
 
 function saveAutoCategorizeSettingFromCookie(value) {
-  document.cookie = `auto_categorized=${value}; path=/`
+  document.cookie = `auto_categorized=${value}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`
 }
 
 function saveSocialInfoToCookie(value) {
